77eff40894143e1dc3a1f167b6de36d0543edd34
Abdussalam Mujeeb-ur-rahman <103681250+Abdussalam-Mujeeb-ur-rahman@users.noreply.github.com>
2023-09-07 23:30:15 +0100

    update 1

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ app.use((err, req, res, next) => {
     next();
 })
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
